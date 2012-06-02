0594c60b279617ca524f0980ae09c25ab31bfe3d
Steven Wilson <wilson.steven10@yahoo.com>
2012-06-02 12:18:38 -0500

    [232] Updated user guide to reflect changes to the system

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 // Define CMS versions
 define('CMS_VERSION', 'Beta 1');
-define('CMS_BUILD', 231);
+define('CMS_BUILD', 232);
 define('REQ_DB_VERSION', '0.16');
 
 // Define a smaller Directory seperater and ROOT path
