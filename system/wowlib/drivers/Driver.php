d9c58b347bb4f3f31789a9b8bbd36c9208a363d0
Wilson212 <wilson.steven10@yahoo.com>
2012-12-16 09:59:54 -0600

    [26] Finished the documentation for the Wowlib Driver class

--- system/wowlib/drivers/Driver.php
+++ system/wowlib/drivers/Driver.php
@@ -1,45 +1,83 @@
 <?php
-/* 
-| --------------------------------------------------------------
-| 
-| WowLib Framework for WoW Private Server CMS'
-|
-| --------------------------------------------------------------
-|
-| Author:       Steven Wilson
-| Copyright:    Copyright (c) 2012, Plexis Dev Team
-| License:      GNU GPL v3
-|
-*/
-
-// All namespace paths must be Uppercase first letter!
+/**
+ * Plexis Content Management System
+ *
+ * @file        System/Wowlib/Drivers/Driver.php
+ * @copyright   2011-2012, Plexis Dev Team
+ * @license     GNU GPL v3
+ * @package     Wowlib
+ * @contains    Driver
+ */
 namespace Wowlib;
 
+// Bring some Plexis CMS core classes into scope
+use \Database\Driver as DBDriver;
+
+/**
+ * This class is used to handle different drivers of all different
+ * emulators
+ *
+ * @author      Steven Wilson
+ * @package     Wowlib
+ */
 class Driver
 {
-    public $CDB;
-    public $WDB;
+    /**
+     * Character database object
+     * @var \Database\Driver
+     */
+    protected $CDB;
+    
+    /**
+     * World database object
+     * @var \Database\Driver
+     */
+    protected $WDB;
+    
+    /**
+     * Driver configuration array
+     * @var mixed[]
+     */
     protected $config;
     
-    // Out wowlib driver and emulator
+    /**
+     * Loaded driver name
+     * @var string
+     */
     protected $driver;
+    
+    /**
+     * Selected emulator name
+     * @var string
+     */
     protected $emulator;
+    
+    /**
+     * Array of loaded driver extensions
+     * @var object[]
+     */
     protected $loaded = array();
     
-
-/*
-| ---------------------------------------------------------------
-| Constructor
-| ---------------------------------------------------------------
-|
-*/
+    
+    /**
+     * Constructor
+     *
+     * @param string $emulator The name of the selected emulator
+     * @param string $driver The name of the driver to load
+     * @param string[] $char An array of database connection for the characters database
+     * @param string[] $world An array of database connection for the world database
+     *
+     * @return void
+     */
     public function __construct($emulator, $driver, $char, $world)
     {
         // First, we must load the driver config!
 		$file = path( WOWLIB_ROOT, 'drivers', strtolower($emulator), strtolower($driver), 'Driver.php' );
         
         // If extension doesnt exist, return false
-        if( !file_exists( $file ) ) throw new \Exception('Config file for driver '. $driver .' not found');
+        if(!file_exists($file)) 
+            throw new \Exception('Config file for driver '. $driver .' not found');
+            
         require $file;
         
         // Load the config variables into a local variable
@@ -51,7 +89,7 @@ class Driver
         if(is_array($char))
         {
             try {
-                $this->CDB = new \Wowlib\Database($char);
+                $this->CDB = new DBDriver($char);
             }
             catch(\Exception $e) {
                 $this->CDB = false;
@@ -62,7 +100,7 @@ class Driver
         if(is_array($world))
         {
             try {
-                $this->WDB = new \Wowlib\Database($world);
+                $this->WDB = new DBDriver($world);
             }
             catch(\Exception $e) {
                 $this->WDB = false;
@@ -74,33 +112,30 @@ class Driver
         $this->driver = $driver;
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: driverHasExt()
-| ---------------------------------------------------------------
-|
-| This method returns if the namespaced driver class exists, and
-| has been loaded.
-|
-| @Param: (String) The driver class name
-| @Param: (Bool) $include - Include the class file if the class
-|   hasnt been loaded yet?
-| @Return: (Object)
-|
-*/
+    /**
+     * Returns if the namespaced driver extension class exists, and has
+     * been loaded
+     *
+     * @param string $name The driver classname
+     * @param bool $include If set to false, the class will not be loaded,
+     *   and this method will true if the class file exists only.
+     *
+     * @return bool
+     */
     public function driverHasExt($name, $include = true)
     {
         $name = ucfirst( strtolower($name) );
         $driver = strtolower($this->driver);
         
         // Make sure if isnt loaded already
-        if(in_array($name, $this->loaded)) return $this->loaded[$name];
+        if(in_array($name, $this->loaded)) 
+            return $this->loaded[$name];
         
         // Check for the extension
 		$file = path( WOWLIB_ROOT, 'drivers', $this->emulator, $driver, $name .'.php' );
         
         // If extension doesnt exist, return false
-        if( !file_exists( $file ) )
+        if(!file_exists($file))
         {
             $this->loaded[$name] = false;
             return false;
@@ -118,102 +153,80 @@ class Driver
         return true;
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: getConfig()
-| ---------------------------------------------------------------
-|
-| This method returns the config array
-|
-| @Return: (Array)
-|
-*/
+    /**
+     * Returns the drivers config array
+     *
+     * @return string[]
+     */
     public function getConfig()
     {
         return $this->config;
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: getColumnById()
-| ---------------------------------------------------------------
-|
-| This method returns the column name for the given ID's
-|
-| @Param: (String) $table - The table ID
-| @Param: (String) $col - The column ID ID
-| @Return: (String)
-|
-*/
+    /**
+     * Returns the column name for the given table id
+     *
+     * @param string $table The table ID
+     * @param string $col The table column ID
+     *
+     * @return string|bool
+     */
     public function getColumnById($table, $col)
     {
         // Make sure the config key exists
-        if(!isset($this->config["{$table}Columns"][$col])) return false;
-        
-        return $this->config["{$table}Columns"][$col];
+        return (isset($this->config["{$table}Columns"][$col])) 
+            ? $this->config["{$table}Columns"][$col] 
+            : false;
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: getDriverNamespace()
-| ---------------------------------------------------------------
-|
-| This method returns the namespace to all driver classes
-|
-| @Return: (String)
-|
-*/
+    /**
+     * Returns the namespace to all driver classes
+     *
+     * @return string
+     */
     public function getDriverNamespace()
     {
         return "\\Wowlib\\". ucfirst($this->emulator) ."\\". ucfirst($this->driver);
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: getCDB()
-| ---------------------------------------------------------------
-|
-| This method returns the database connection object to the 
-| characters database
-|
-| @Return: (Object)
-|
-*/
+    /**
+     * Returns the character database connection object
+     *
+     * @return \Database\Driver|bool Return false only if the
+     *   character database connection fails
+     */
     public function getCDB()
     {
         return $this->CDB;
     }
     
-/*
-| ---------------------------------------------------------------
-| Method: getWDB()
-| ---------------------------------------------------------------
-|
-| This method returns the database connection object to the 
-| world database
-|
-| @Return: (Object)
-|
-*/
+    /**
+     * Returns the world database connection object
+     *
+     * @return \Database\Driver|bool Return false only if the
+     *   world database connection fails
+     */
     public function getWDB()
     {
         return $this->WDB;
     }
     
-/*
-| ---------------------------------------------------------------
-| Extenstion loader
-| ---------------------------------------------------------------
-|
-*/
+    /**
+     * Magic method used to automatically load called extensions
+     *
+     * @param string $name The name of the requested extension
+     *
+     * @return object|bool
+     */
     public function __get($name)
     {
         // Just return the extension if it exists
         $name = strtolower($name);
-        if(isset($this->{$name})) return $this->{$name};
+        if(isset($this->{$name}))
+            return $this->{$name};
         
         // Create our classname
-        $class = ucfirst( $name );
+        $class = ucfirst($name);
         $driver = strtolower($this->driver);
         $ucEmu = ucfirst($this->emulator);
         
@@ -221,10 +234,11 @@ class Driver
 		$file = path( WOWLIB_ROOT, 'library', $class .'.php' );
         
         // If extension doesnt exist, return false
-        if( !file_exists( $file ) ) return false;
+        if(!file_exists($file)) 
+            return false;
         
         // Load the extension file
-        require_once( $file );
+        require_once($file);
         
         // Load the class
         $class = "\\Wowlib\\{$class}";
